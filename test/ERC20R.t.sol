3a59c1ffef354d99211426a1c6ddcf2d3148ddc7
shivam2320 <alekarshivam23@gmail.com>
2023-02-02 16:18:34 +0530

    test: recurringApprove

--- test/ERC20R.t.sol
+++ test/ERC20R.t.sol
@@ -8,7 +8,32 @@ import "src/ERC20R.sol";
 contract ERC20RTest is Test {
     ERC20R erc20r;
 
-    function setup() public {
+    address owner = address(0x1);
+    address spender = address(0x2);
+
+    struct Recurring {
+        uint256 allowedAmount;
+        uint256 timePeriod;
+        uint256 timeLimit;
+        uint256 nextInterval;
+    }
+
+    function setUp() public {
         erc20r = new ERC20R("TEST", "TST", 18);
+    }
+
+    function testrecurringApprove() public {
+        vm.startPrank(owner);
+        erc20r.recurringApprove(spender, 100, 86400, 2629743);
+        (
+            uint256 allowedAmount,
+            uint256 timePeriod,
+            uint256 timeLimit,
+            uint256 nextInterval
+        ) = erc20r.recurringAllowance(owner, spender);
+        assertEq(allowedAmount, 100);
+        assertEq(timePeriod, 86400);
+        assertEq(timeLimit, block.timestamp + 2629743);
+        assertEq(nextInterval, block.timestamp);
     }
 }
